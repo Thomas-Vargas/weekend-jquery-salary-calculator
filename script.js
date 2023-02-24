46781d63ac03dd05b0ba994204576a1bb5b42d25
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-02-24 09:15:51 -0600

    Inputs now required, each input val is added to an object and then pushed to an employees array.

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 console.log('js loaded')
 
+let employees = [];
+
+$(document).ready(readyNow)
+
+function readyNow() {
+    $('#submit').on('click', submit)
+}
+
+function submit() {
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
+    let employeeID = $('#employee-id').val();
+    let employeeTitle = $('#employee-title').val();
+    let annualSalary = $('#annual-salary').val();
+
+    console.log(firstName, lastName, employeeID, employeeTitle, annualSalary)
+
+    if (firstName && lastName && employeeID && employeeTitle && annualSalary) {
+        let employeeObject = {
+            firstName: firstName,
+            lastName: lastName,
+            employeeID: employeeID,
+            employeeTitle: employeeTitle,
+            annualSalary: annualSalary
+        }
+
+        //console.log(employeeObject)
+        employees.push(employeeObject);
+    }
+    else {
+        console.log('invalid')
+        alert('must fill all inputs')
+    }
+
+    console.log(employees)
+    clearInputs()
+    render()
+}
+
+
+
+function clearInputs() {
+    let firstName = $('#first-name').val('');
+    let lastName = $('#last-name').val('');
+    let employeeID = $('#employee-id').val('');
+    let employeeTitle = $('#employee-title').val('');
+    let annualSalary = $('#annual-salary').val('');
+}
